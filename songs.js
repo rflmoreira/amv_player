ebd29a9d7b0f3b76b67134c6fceb151e8513db30
rflmoreira <rafaelmoreira.lima@hotmail.com>
2025-06-10 23:31:11 -0300

    ajustes específicos

--- songs.js
+++ songs.js
@@ -9,21 +9,21 @@ export default [
     name: "[AMV] - WARRIORS",
     author: "viberonin",
   },
-  {
-    src: "src/videos/Boruto AMV - Ashes~On My Own.mp4",
-    name: "Boruto AMV - Ashes~On My Own",
-    author: "Indie Otaku",
-  },
+  // {
+  //   src: "src/videos/Boruto AMV - Ashes~On My Own.mp4",
+  //   name: "Boruto AMV - Ashes~On My Own",
+  //   author: "Indie Otaku",
+  // },
   {
     src: "src/videos/Skyfall  - Demon Slayer [AMV].mp4",
     name: "Skyfall - Demon Slayer [AMV]",
     author: "tiff.",
   },
-  {
-    src: "src/videos/Pantheon (2022) - AMV.mp4",
-    name: "Pantheon (2022) - AMV",
-    author: "Knight Camelot",
-  },
+  // {
+  //   src: "src/videos/Pantheon (2022) - AMV.mp4",
+  //   name: "Pantheon (2022) - AMV",
+  //   author: "Knight Camelot",
+  // },
   {
     src: "src/videos/Anime Mix「AMV」- Whatever It Takes.mp4",
     name: "Anime Mix「AMV」- Whatever It Takes",
