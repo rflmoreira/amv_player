10db1cf0bb3340d764500da72b21493047c9bd3b
rflmoreira <rafaelmoreira.lima@hotmail.com>
2025-07-20 23:07:45 -0300

    Alteração na estrutura de reprodução.

--- songs.js
+++ songs.js
@@ -5,32 +5,32 @@ export default [
     author: " artista sumido",
   },
   {
-    src: "src/videos/[AMV] - WARRIORS.mp4",
+    src: "https://pub-49ebc8fe985e463d90f8aa912454bf50.r2.dev/video/%5BAMV%5D%20-%20WARRIORS.mp4",
     name: "[AMV] - WARRIORS",
     author: "viberonin",
   },
   {
-    src: "src/videos/Skyfall  - Demon Slayer [AMV].mp4",
+    src: "https://pub-49ebc8fe985e463d90f8aa912454bf50.r2.dev/video/Skyfall%20%20-%20Demon%20Slayer%20%5BAMV%5D.mp4",
     name: "Skyfall - Demon Slayer [AMV]",
     author: "tiff.",
   },
   {
-    src: "src/videos/running up that hill, a deal with god - evangelion amv.mp4",
+    src: "https://pub-49ebc8fe985e463d90f8aa912454bf50.r2.dev/video/running%20up%20that%20hill%2C%20a%20deal%20with%20god%20-%20evangelion%20amv.mp4",
     name: "running up that hill, a deal with god - evangelion amv",
     author: "lilin",
   },
   {
-    src: "src/videos/Anime Mix「AMV」- Whatever It Takes.mp4",
+    src: "https://pub-49ebc8fe985e463d90f8aa912454bf50.r2.dev/video/Anime%20Mix%E3%80%8CAMV%E3%80%8D-%20Whatever%20It%20Takes.mp4",
     name: "Anime Mix「AMV」- Whatever It Takes",
     author: "Royal Grindi",
   },
   {
-    src: "src/videos/AMV ｜ Tanjiro (Demon Slayer) - Gasoline (Halsey).mp4",
+    src: "https://pub-49ebc8fe985e463d90f8aa912454bf50.r2.dev/video/AMV%20%EF%BD%9C%20Tanjiro%20(Demon%20Slayer)%20-%20Gasoline%20(Halsey).mp4",
     name: "AMV ｜ Tanjiro (Demon Slayer) - Gasoline (Halsey)",
     author: "mocha amv",
   },
   {
-    src: "src/videos/Darkside「AMV」Anime Mix.mp4",
+    src: "https://pub-49ebc8fe985e463d90f8aa912454bf50.r2.dev/video/Darkside%E3%80%8CAMV%E3%80%8DAnime%20Mix.mp4",
     name: "Darkside「AMV」Anime Mix",
     author: "UnClonable",
   },
